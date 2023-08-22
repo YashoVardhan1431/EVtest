766a5ed8c8f1743424332ada2ed571a94f9869ec
Yasho <yashokandregula@gmail.com>
2023-08-23 00:22:15 +0530

    second commit

--- routes/resultRoute.js
+++ routes/resultRoute.js
@@ -1,11 +1,10 @@
 const  express = require('express')
 
-const customerCommunication = require("../controllers/CCH")
-const callRecordData = require("../controllers/CCH")
+const CCH = require("../controllers/CCH")
 
 const router = express.Router();
 
-router.get('/masterData', customerCommunication)
-router.get('/callData', callRecordData)
+router.get('/masterData', CCH.customerCommunication)
+router.get('/callData', CCH.callRecordData)
 
 module.exports = router;
